b6b5d4ee969af939ca2f16a96dbd892a0676fbd9
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2023-05-14 18:17:31 +0900

    [feature]天気ボタン追加

--- src/extension.ts
+++ src/extension.ts
@@ -4,38 +4,85 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   console.log('"trash-plugin" is now active!');
 
-  // コマンドを作成
-  let disposable = vscode.commands.registerCommand(
-    "trash-plugin.happy",
-    omikuzi
-  );
+  // おみくじを引く機能を登録
+  registerCMD("trash-plugin.omikuzi", omikuzi, context);
 
-  // 作成したコマンドを登録
+  // 天気を選ぶ機能を登録
+  registerCMD("trash-plugin.weather", weather, context);
+
+  // おみくじボタンを配置
+  registerButton("今日の運勢は", "trash-plugin.omikuzi", context);
+
+  // おみくじボタンを配置
+  registerButton("今日の天気は", "trash-plugin.weather", context);
+}
+
+/**
+ * コマンドを登録する処理です。
+ * @param name コマンドの名前
+ * @param func コマンド実行時の処理
+ * @param context vscode拡張機能のcontext
+ * @return なし
+ */
+const registerCMD = (
+  name: string,
+  func: any,
+  context: vscode.ExtensionContext
+) => {
+  const disposable = vscode.commands.registerCommand(name, func);
   context.subscriptions.push(disposable);
+};
 
-  // 右下にボタンを作成
+/**
+ * ボタンを登録する処理です。
+ * @param label ボタンのラベル
+ * @param commandName 実行するコマンド名
+ * @param context vscode拡張機能のcontext
+ * @return なし
+ */
+const registerButton = (
+  label: string,
+  commandName: string,
+  context: vscode.ExtensionContext
+) => {
   const button = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Right,
     0
   );
 
   // ボタンの内容を記載
-  button.command = "trash-plugin.happy";
-  button.text = "今日の運勢は";
+  button.command = commandName;
+  button.text = label;
 
   // ボタンを登録
   context.subscriptions.push(button);
 
   // ボタンを表示
   button.show();
-}
+};
 
-// おみくじの処理
+/**
+ * ランダムにおみくじの情報をポップアップする処理
+ * @param なし
+ * @return なし
+ */
 const omikuzi = () => {
   const lists = ["大凶", "凶", "小吉", "吉", "中吉", "大吉"];
   const result = lists[Math.floor(Math.random() * lists.length)];
   vscode.window.showInformationMessage(`今日の運勢は${result}です！`);
 };
 
-// プラグイン削除時?の挙動
+/**
+ * ランダムに天気の情報をポップアップする処理
+ * @param なし
+ * @returns なし
+ */
+const weather = () => {
+  const lists = ["雨", "くもり", "晴れ", "ひょう", "霧"];
+  const result = lists[Math.floor(Math.random() * lists.length)];
+  vscode.window.showInformationMessage(
+    `今日の天気は${result}です！違ったらごめんね`
+  );
+};
+
 export function deactivate() {}
